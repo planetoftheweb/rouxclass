6332c5bd0bac97fd1342de29eba6fdc7444475c8
Ray Villalobos <ray@planetoftheweb.com>
2014-05-20 11:30:24 -0700

    Added Express & Routes

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-var http = require('http'); //add the http module
-var myServer = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type" : "text/html"});
-  response.write("<b>Hello</b> World");
-  response.end();
-}); //create a server
+var express = require('express');
+var app = express();
 
-myServer.listen(3000);
+app.get('/', function(req, res) {
+  res.send('<H1>Hello</H1> Express');
+});
 
-console.log("Go to http://localhost:3000 on your browser");
+app.get('/me', function(req, res) {
+  res.send('@planetoftheweb');
+});
+
+app.get('/who/:name?', function(req, res) {
+  var name = req.params.name;
+  res.send(name + ' was here');
+});
+
+app.get('/who/:name?/:title?', function(req, res) {
+  var name = req.params.name;
+  var title = req.params.title;
+  res.send('<p>name: ' + name + '<br>title: ' + title + '</p>');
+});
+
+app.get('*', function(req, res) {
+  res.send('Bad Route');
+});
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
